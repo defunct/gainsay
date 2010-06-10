2e4e0b7d549abe8a7c9b2db177894d833f6b739e
Alan Gutierrez <alan@blogometer.com>
2010-06-09 20:45:19 -0500

    Update Mix build after refactor of Mix.

--- src/mix/java/com/goodworkalan/gainsay/mix/GainsayProject.java
+++ src/mix/java/com/goodworkalan/gainsay/mix/GainsayProject.java
@@ -1,20 +1,27 @@
 package com.goodworkalan.cassandra.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
-import com.goodworkalan.mix.builder.JavaProject;
+import com.goodworkalan.mix.cookbook.JavaProject;
 
-public class GainsayProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Gainsay.
+ *
+ * @author Alan Gutierrez
+ */
+public class GainsayProject implements ProjectModule {
+    /**
+     * Build the project definition for Gainsay.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/gainsay/0.1"))
-                .test()
-                    .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
-                        .end()
+                .produces("com.goodworkalan/gainsay/0.1")
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
